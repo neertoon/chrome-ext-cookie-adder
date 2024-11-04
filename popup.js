9e7d07d212050e80ffe5b6ce866f7cbd537aa22e
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-11-04 08:01:29 +0100

    Zmiana wyglądu przełaczania cookie, dodanie wersji prod local

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+let textProd = "PROD";
+let textLocal = "LOCAL";
+let cookieName = 'cookie-name';
+let cookieValue = 'cookie-value';
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("cookie-button");
 
@@ -7,27 +11,30 @@ document.addEventListener("DOMContentLoaded", () => {
             const tab = tabs[0];
             const url = new URL(tab.url);
 
-            chrome.cookies.get({ url: url.origin, name: "exampleCookie" }, (cookie) => {
+            chrome.cookies.get({ url: url.origin, name: cookieName }, (cookie) => {
                 if (cookie) {
-                    button.textContent = "USUN CIASTECZKO";
+                    button.textContent = textProd;
+                    gcCookieAddProdClass(button)
                     button.onclick = () => {
-                        chrome.cookies.remove({ url: url.origin, name: "exampleCookie" }, (details) => {
+                        chrome.cookies.remove({ url: url.origin, name: cookieName }, (details) => {
                             if (chrome.runtime.lastError) {
                                 console.error(chrome.runtime.lastError.message);
                             } else {
                                 console.log("Cookie removed:", details);
-                                alert("Ciasteczko zostało usunięte!");
-                                button.textContent = "Dodaj ciasteczko"; // Zmiana tekstu po usunięciu
+                                // alert("Ciasteczko zostało usunięte!");
+                                button.textContent = textLocal; // Zmiana tekstu po usunięciu
+                                gcCookieAddLocalClass(button);
                             }
                         });
                     };
                 } else {
-                    button.textContent = "DODAJ CIASTECZKO";
+                    button.textContent = textLocal;
+                    gcCookieAddLocalClass(button);
                     button.onclick = () => {
                         chrome.cookies.set({
                             url: url.origin,
-                            name: "exampleCookie",
-                            value: "exampleValue",
+                            name: cookieName,
+                            value: cookieValue,
                             domain: url.hostname,
                             path: "/",
                             secure: true,
@@ -38,8 +45,9 @@ document.addEventListener("DOMContentLoaded", () => {
                                 console.error(chrome.runtime.lastError.message);
                             } else {
                                 console.log("Cookie added:", cookie);
-                                alert("Dodano ciasteczko!");
-                                button.textContent = "Usuń ciasteczko"; // Zmiana tekstu po dodaniu
+                                // alert("Dodano ciasteczko!");
+                                button.textContent = textProd; // Zmiana tekstu po dodaniu
+                                gcCookieAddProdClass(button)
                             }
                         });
                     };
@@ -48,3 +56,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+function gcCookieAddLocalClass(button) {
+    button.classList.remove('prod');
+    button.classList.add('local');
+}
+
+function gcCookieAddProdClass(button) {
+    button.classList.remove('local');
+    button.classList.add('prod');
+}
